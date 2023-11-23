270519a578008f728da141afd92ac4b624e94b55
AntKhom <antonhomenko1988@gmail.com>
2023-11-23 07:32:59 -0500

    Update selectors.jsx

--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,11 +1,8 @@
 export const selectContacts = state => state.contacts.contacts;
-
 export const selectIsLoading = state => state.contacts.isLoading;
-
 export const selectError = state => state.contacts.error;
 
 export const selectContactsFilter = state => state.filter.filterQuery;
-
 export const selectFilteredContacts = state => {
   const contacts = selectContacts(state);
   const filterQuery = selectContactsFilter(state);
