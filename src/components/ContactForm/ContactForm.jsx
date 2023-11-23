a0bf477556a4177288eb021870547faaad2f4875
AntKhom <antonhomenko1988@gmail.com>
2023-11-23 07:36:12 -0500

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,7 +30,7 @@ const ContactForm = () => {
         <br />      
         <input
                 type="tel"
-                name="number"
+                name="phone"
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 placeholder="Phone number"
